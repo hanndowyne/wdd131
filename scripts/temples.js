765c1887647355c6a33df01991c6ffc8a5798f8e
hanndowyne <valcourtbercy2004@gmail.com>
2025-09-15 22:17:57 -0400

    Added temples.html

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = `© ${currentYear}`;
+
+const lastModified = document.lastModified;
+
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
