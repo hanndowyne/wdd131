b21c1e4ac13fcb2b2f3b53b717ba9c58cbaf6fcb
Hann Dowyne Valcourt <152522603+hanndowyne@users.noreply.github.com>
2025-09-15 20:28:40 -0400

    Delete home directory
    wrong directory

--- home/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-const currentYear = new Date().getFullYear();
-
-document.getElementById("currentyear").textContent = `© ${currentYear}`;
-
-const lastModified = document.lastModified;
-
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
