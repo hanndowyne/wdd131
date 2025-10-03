70a963bc170d8257879e6cc4373ffe5c9699cc27
hanndowyne <valcourtbercy2004@gmail.com>
2025-10-03 17:20:05 -0400

    modified file

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,18 @@
 const products = [
-  { id: "p1", name: "Laptop Pro X" },
-  { id: "p2", name: "Smartphone Galaxy" },
-  { id: "p3", name: "Wireless Headphones" },
-  { id: "p4", name: "Anime Poster Pack" },
-  { id: "p5", name: "Gaming Mouse" }
+  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
 ];
 
 
-const productSelect = document.querySelector("#productName");
+const productSelect = document.getElementById("productName");
 
 products.forEach(product => {
   const option = document.createElement("option");
-  option.value = product.name;
-  option.textContent = product.name;
+  option.value = product.id;     
+  option.textContent = product.name; 
   productSelect.appendChild(option);
 });
+
