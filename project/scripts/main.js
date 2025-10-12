12b68de14b59e54995d7367b6118c71ad2b4c1bc
hanndowyne <valcourtbercy2004@gmail.com>
2025-10-11 21:31:18 -0400

    modified file

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -1,26 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
- 
   const menuButton = document.querySelector('.menu-toggle');
   const navLinks = document.querySelector('.nav-links');
+  const yearSpan = document.getElementById('year');
+
 
   if (menuButton && navLinks) {
     menuButton.addEventListener('click', () => {
       navLinks.classList.toggle('show');
+
+      
+      if (navLinks.classList.contains('show')) {
+        navLinks.style.maxHeight = navLinks.scrollHeight + "px";
+      } else {
+        navLinks.style.maxHeight = null;
+      }
     });
   }
 
-  document.querySelector('.menu-toggle').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('show');
-});
-
-
-document.getElementById('year').textContent = new Date().getFullYear();
-
-  const yearSpan = document.getElementById("year");
+ 
   if (yearSpan) {
     yearSpan.textContent = new Date().getFullYear();
   }
 
+  
   const hour = new Date().getHours();
   const message = hour < 12 ? "Good morning, anime lover!" : "Good evening, anime fan!";
   console.log(message);
